bb325271b329873aa48b463be9fd922eced5809e
Host-Info.net <50676042+aerobotpro@users.noreply.github.com>
2020-05-06 11:28:41 -0700

    Update glbls.py

--- src/glbls.py
+++ src/glbls.py
@@ -1,13 +1,10 @@
-
-
-
 class glbls:
     # Your user-token goes in 'tokens.txt'.
     TOKEN = open("data/token.txt").read()
     # You can set  your prefix below.
     prefix = "--"
     # You can set your current Rich Presence below: 
-    presence = "pornhub"
+    presence = "Netflix" # Action: "Watching"
     
     chars = ['a','b','c','d','e','f','1','2','3','4','5','6','7','8','9','0']
     random_remarks = ["Ayoo!", "Whats poppin?!", "Hey all you cool cats and kittens!", "Oye Matey!"]
